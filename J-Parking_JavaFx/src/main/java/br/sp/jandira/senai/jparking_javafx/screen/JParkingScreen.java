4f86122f7f0d4d011e9870f9cc6953f9d2c192ac
JEAN COSTA ALVES DA SILVA <25203640@dsn127m1242313.cfp127aluno.local>
2025-12-05 10:11:38 -0300

    Adicionando Stages

--- J-Parking_JavaFx/src/main/java/br/sp/jandira/senai/jparking_javafx/screen/JParkingScreen.java
+++ J-Parking_JavaFx/src/main/java/br/sp/jandira/senai/jparking_javafx/screen/JParkingScreen.java
@@ -1,11 +1,14 @@
 package br.sp.jandira.senai.jparking_javafx.screen;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane; // Usando Pane vazio por enquanto
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class JParkingScreen extends Application {
@@ -13,15 +16,31 @@ public class JParkingScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Pane root = new Pane();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-
         stage.setWidth(660);
         stage.setHeight(700);
         stage.setResizable(false);
         stage.setTitle("J-Parking");
 
+        try {
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/J-parking.logo.png")));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.out.println("Erro: Imagem do ícone não encontrada. Verifique se moveu para 'resources'.");
+            e.printStackTrace();
+        }
+
+        VBox root = new VBox();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
         stage.show();
+
+        VBox header = new VBox();
+        header.setStyle("-fx-padding: 10; -fx-background-color:F8F8F8 ");
+
+
+
+
+
     }
 }
